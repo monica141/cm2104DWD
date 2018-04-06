f01a041bb611822ad8ec0e20d75d4ca78298d966
sergiocast-97 <sergiocast97@gmail.com>
2018-04-06 11:30:18 +0100

    commit

---
+++ Lecture 04 - JavaScript/errors.js
@@ -0,0 +1,16 @@
+function pain () {
+	var a;
+	var b;
+	var c;
+	
+	a = 10;
+	b = 100;
+	
+	b = a + b;
+	c = a * b;
+	
+	// Print the result in the page
+	document.writeln(c);
+	// Console will output the return
+	return c;
+}
