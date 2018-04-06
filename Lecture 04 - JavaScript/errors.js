e6af26b50b1afb92d4e4b7114a2e27cd1d17b9b8
sergiocast-97 <sergiocast97@gmail.com>
2018-04-06 18:14:26 +0100

    labs

--- Lecture 04 - JavaScript/errors.js
+++ Lecture 04 - JavaScript/errors.js
@@ -2,13 +2,13 @@ function pain () {
 	var a;
 	var b;
 	var c;
-	
+
 	a = 10;
 	b = 100;
-	
+
 	b = a + b;
 	c = a * b;
-	
+
 	// Print the result in the page
 	document.writeln(c);
 	// Console will output the return
