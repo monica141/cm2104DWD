4c72c2c2cb667b14516651ffeb8875433e988117
sergiocast-97 <sergiocast97@gmail.com>
2018-04-11 15:39:29 +0100

    JavaScript

--- Lecture 04 - JavaScript/catspin.js
+++ Lecture 04 - JavaScript/catspin.js
@@ -1,7 +1,7 @@
 /**
  * Created by john on 10/03/2016.
  */
-
+/*
 var cat = document.querySelector("img");
 var angle = 0, lastTime = null;
 function animate(time) {
@@ -13,5 +13,76 @@ function animate(time) {
     requestAnimationFrame(animate);
 }
 requestAnimationFrame(animate);
+*/
 
+// Selecting the cat image
+var cat = document.querySelector("img");
+// Setting the timer as null
+var lastTime = null;
+// Setting the increase, initially 0
+var yValue = 0 , xValue = 0;
+// Flag for the image touching the edges
+var touchedBottom = false, touchedRight = false;
 
+// Animation function
+function animate( time ){
+
+   // Making sure the animation starts after the first frame
+   if (lastTime != null){
+      // If the image has touched the bottom
+      if(touchedBottom){
+         // Decrease the Y value
+         yValue -= 5;
+      // If the image touches the top
+      } else {
+         // Increase the Y value
+         yValue += 5;
+      }
+      // If the image touches the right edge
+      if(touchedRight){
+         // Decrease the X value
+         xValue -= 5;
+      // If the image touches the left edge
+      } else {
+         // If the image touches the right edge
+         xValue += 5;
+      }
+   }
+   // Increasing the frames
+   lastTime = time;
+
+   // If the Y value is more than the screen height
+   if(yValue+100 > screen.availHeight ){
+      // Set the bottom flag as true
+      touchedBottom = true;
+   }
+   // If the Y value is less than 0
+   if( yValue < 0){
+      // Set the bottom flag as false
+      touchedBottom = false;
+   }
+   // If the X value is more than the screen width
+   if( xValue+100 > screen.width){
+      // Set the right flag as true
+      touchedRight = true;
+   }
+   // If the X value is more than the screen width
+   if( xValue < 0){
+      // Set the right flag as false
+      touchedRight = false;
+   }
+
+   // Increase the Y axis of the image position
+   cat.style.top = yValue + "px";
+   // Increase the X of the image position
+   cat.style.left = xValue + "px";
+
+   // Making the animation endlessly recursive
+   cat.onclick = function(){
+      cat.style.width = "200px";
+      cat.style.height = "200px";
+   }
+   requestAnimationFrame(animate);
+}
+// Calling the function initially
+requestAnimationFrame(animate);
--- Lecture 04 - JavaScript/contentlist.js
+++ Lecture 04 - JavaScript/contentlist.js
@@ -1,8 +1,9 @@
 
 function addContent () {
+
 	// add a list of items to the content div
 	var items = ["hewey", "dewey", "louie"];
-	
+
 	// build the html string for a <ul> list
 	var items_html = "<ul>";
 	for (var i=0; i < items.length; i++) {
@@ -10,10 +11,42 @@ function addContent () {
 		items_html += "<li>" + item + "</li>";
 	};
 	items_html += "</ul>";
-	
+
+
 	// using javascript
 	// 1. find the content div
+	var content = document.getElementById('content');
+
 	// 2. modify its html attribute by adding items_html
 
+	// Inner HTML method
+	content.innerHTML = items_html;
+
+	// Append child method (BAD)
+	//text = document.createTextNode(items_html);
+	//content.appendChild(text);
+
+	// Write code
+	//document.write(items_html);
 }
 
+function deleteContent(){
+
+	var content = document.getElementById('content');
+
+	content.innerHTML = "";
+}
+
+function writePhrase(){
+
+	phraseToWrite = document.getElementById("phraseToWrite");
+	console.log( phraseToWrite.toString() );
+
+	phrase = phraseToWrite.value;
+	console.log( phrase );
+
+	var textElement = document.createElement("p");
+	var text = document.createTextNode( phrase );
+	content.appendChild(text);
+
+}
