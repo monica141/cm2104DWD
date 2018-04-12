105b4a161b431a086727d06bca8647529489b6a3
Sergio Castillo <sergiocast97@gmail.com>
2018-04-12 18:02:03 +0000

    express

---
+++ Lecture 07 - Node Js/Express/server.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var app = express();
+var Twitter = require('twitter');
+var client = new Twitter({
+    consumer_key: 'iyddfRPSfExEXQo5kkeWD5wyE',
+    consumer_secret: 'xKD4M8v4JkDjJhGoxmoCBzsIEtxgOBXv9X4sxrFI1VuZedxxOu',
+    access_token_key: '545491923-WS0cwMPvuub8zDIqszZZPecI5DP63bVBHD2I9DFw',
+    access_token_secret: 'AZSSk35qnGNEyrsYQ79od5ZZmA744AnPuOMymT63N6cL6'
+});
+//console.log( client );
+
+app.use(express.static('public'));
+
+app.get('/', function(req, res){
+    var params = {screen_name: 'nodejs'};
+    //console.log( params );
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+        if (!error) {
+            //console.log(tweets);
+            var output = "";
+            for (var t = 0; t < tweets.length; t++) {
+            output += "<div>";
+            output += "<h2>" + tweets[t].user.screen_name + "<h2>";
+            output += "<p>" + tweets[t].text + "</p>"
+            output += "</div>";
+            }
+            res.send(output);
+        }
+    });
+    
+});
+
+app.listen(8080);
